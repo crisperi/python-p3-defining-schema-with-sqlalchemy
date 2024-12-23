ea44289a2a33bdc750e9dfe2cedb74e58631f9c9
coder-w <phoenixedian@gmail.com>
2024-12-23 04:51:52 +0300

    done

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,21 @@
-#!/usr/bin/env python3
+from sqlalchemy import create_engine
 
+
+
+#!/usr/bin/env python3
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
+
 class Student(Base):
-    pass
+    _tablename_ = 'students'
+    
+    id=Column(Integer, primary_key=True)
+    name =Column(String())
 
 if __name__ == '__main__':
-    pass
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata_create_all(engine)
